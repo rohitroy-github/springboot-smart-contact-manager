6ec0dad7e4bd11a965891e05dfd89df7e6379912
Roy <rohit.f.roy@accenture.com>
2024-11-12 19:52:36 +0530

    add: adding base files

--- src/main/java/springdev/scm/controllers/PageController.java
+++ src/main/java/springdev/scm/controllers/PageController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class PageController {
 
+    // routing for /home
     @RequestMapping("/home")
-    public String homePage(Model model) { 
+    public String homePage(Model model) {
 
         System.out.println("Home Page");
         model.addAttribute("name", "Rohit");
@@ -16,8 +17,9 @@ public class PageController {
 
     }
 
+    // routing for /about
     @RequestMapping("/about")
-    public String aboutPage(Model model) { 
+    public String aboutPage(Model model) {
 
         System.out.println("About Page");
         model.addAttribute("name", "Rohit");
@@ -25,8 +27,9 @@ public class PageController {
 
     }
 
+    // routing for /services
     @RequestMapping("/services")
-    public String servicePage(Model model) { 
+    public String servicePage(Model model) {
 
         System.out.println("Service Page");
         model.addAttribute("name", "Rohit");
