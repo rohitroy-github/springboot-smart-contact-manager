8d95576c94eb2da15bb710d120197007ed9fba16
Roy <rohit.f.roy@accenture.com>
2024-11-14 12:23:30 +0530

    add: new pages

--- src/main/java/springdev/scm/entities/SocialLink.java
+++ src/main/java/springdev/scm/entities/SocialLink.java
@@ -2,6 +2,8 @@ package springdev.scm.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -22,6 +24,7 @@ public class SocialLink {
 
     @Id
     @Column(name = "id", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "link", nullable = false, length = 255)
