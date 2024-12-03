caf31bcebd5a3d728d01bc34e2b2057893c69613
rohitroy@github <rhtry.tech@gmail.com>
2024-12-03 18:01:35 +0530

    update: new design in sidebar

---
+++ src/main/java/springdev/scm/controllers/ContactController.java
@@ -0,0 +1,15 @@
+package springdev.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user/contact")
+public class ContactController {
+
+    @RequestMapping("/add")
+    public String addContactView() { 
+        return "user/add_contact";
+    }
+
+}
