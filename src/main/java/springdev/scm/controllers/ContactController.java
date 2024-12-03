b6f7a12f9fc9c5caa2917fb4e5431127e5daf5c8
rohitroy@github <rhtry.tech@gmail.com>
2024-12-03 20:01:20 +0530

    add: add contact page

--- src/main/java/springdev/scm/controllers/ContactController.java
+++ src/main/java/springdev/scm/controllers/ContactController.java
@@ -2,14 +2,20 @@ package springdev.scm.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("user/contact")
 public class ContactController {
 
-    @RequestMapping("/add")
+    @RequestMapping(value = "add")
     public String addContactView() { 
         return "user/add_contact";
+    }
+
+    @RequestMapping(value = "add-new-contact", method = RequestMethod.POST)
+    public String processAddNewContact() { 
+        return "";
     }
 
 }
