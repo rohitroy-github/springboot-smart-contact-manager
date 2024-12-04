7b9a4eb25b493f78872f6d74e0e8c82983738b5d
rohitroy@github <rhtry.tech@gmail.com>
2024-12-04 15:00:33 +0530

    update: refactored the add_contact form

--- src/main/java/springdev/scm/controllers/ContactController.java
+++ src/main/java/springdev/scm/controllers/ContactController.java
@@ -1,15 +1,22 @@
 package springdev.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import springdev.scm.forms.ContactForm;
 
 @Controller
 @RequestMapping("user/contact")
 public class ContactController {
 
     @RequestMapping(value = "add")
-    public String addContactView() { 
+    public String addContactView(Model model) { 
+
+        ContactForm contactForm = new ContactForm();
+        model.addAttribute("contactFormData", contactForm);
+
         return "user/add_contact";
     }
 
---
+++ src/main/java/springdev/scm/forms/ContactForm.java
@@ -0,0 +1,30 @@
+package springdev.scm.forms;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class ContactForm {
+
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String address;
+    private String description;
+    private String websiteLink;
+    private String linkedInLink;
+    private boolean favourite;
+    private MultipartFile profileImage;
+
+}
