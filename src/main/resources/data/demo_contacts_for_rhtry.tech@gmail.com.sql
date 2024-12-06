6d206dc8e6fbe2543a07a56b554fd400a2562cb2
rohitroy@github <rhtry.tech@gmail.com>
2024-12-06 13:55:16 +0530

    add: adding demo data sql script & refactoring of contacts page

---
+++ src/main/resources/data/demo_contacts_for_rhtry.tech@gmail.com.sql
@@ -0,0 +1,36 @@
+INSERT INTO contacts (
+    contact_id, 
+    address, 
+    description, 
+    email, 
+    favourite, 
+    linkedin_link, 
+    name, 
+    phone_number, 
+    picture, 
+    website_link, 
+    user_user_id
+) VALUES
+('2580f75a-4aac-4527-a20a-db0654b6cbaa', 'testaddress', 'this is test entry', 'test@gmail.com', b'1', 'linktr.ee/rohitroy_r', 'test', '1234', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/rohitroy_r', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('5b7e3a41-d8b6-45c1-a2dc-df89339b8a8e', 'address1', 'Demo contact 1', 'demo1@gmail.com', b'0', 'linktr.ee/demo1', 'Alice', '9876543210', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo1', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('a2b1839c-8f2f-4a94-9255-b0bfc0ddc793', 'address2', 'Demo contact 2', 'demo2@gmail.com', b'0', 'linktr.ee/demo2', 'Bob', '8765432109', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo2', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('c3f9289f-7b6d-4634-87f7-ea52ad5878b7', 'address3', 'Demo contact 3', 'demo3@gmail.com', b'1', 'linktr.ee/demo3', 'Charlie', '7654321098', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo3', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('d6a47c7f-3b59-45a4-88ad-687ab2d5d211', 'address4', 'Demo contact 4', 'demo4@gmail.com', b'1', 'linktr.ee/demo4', 'Diana', '6543210987', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo4', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('e9b16c78-4f48-47df-8f24-1d5f50cf8b6d', 'address5', 'Demo contact 5', 'demo5@gmail.com', b'0', 'linktr.ee/demo5', 'Eve', '5432109876', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo5', '689444e0-6000-4107-99df-d8335e4f9a8c');
