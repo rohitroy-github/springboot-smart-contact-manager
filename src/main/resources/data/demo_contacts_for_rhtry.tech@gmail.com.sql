c34cb0233799665e800b93f7f7a9ab0642f263b9
rohitroy@github <rhtry.tech@gmail.com>
2024-12-06 17:44:03 +0530

    update: added some more demo data to test pagination

--- src/main/resources/data/demo_contacts_for_rhtry.tech@gmail.com.sql
+++ src/main/resources/data/demo_contacts_for_rhtry.tech@gmail.com.sql
@@ -33,4 +33,43 @@ INSERT INTO contacts (
 
 ('e9b16c78-4f48-47df-8f24-1d5f50cf8b6d', 'address5', 'Demo contact 5', 'demo5@gmail.com', b'0', 'linktr.ee/demo5', 'Eve', '5432109876', 
 'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
-'linktr.ee/demo5', '689444e0-6000-4107-99df-d8335e4f9a8c');
+'linktr.ee/demo5', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+('f1a8e2c9-3e97-4e8d-a3d4-1e48b9271b23', 'address6', 'Demo contact 6', 'demo6@gmail.com', b'1', 'linktr.ee/demo6', 'Frank', '4321098765', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo6', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('ab3cd1ef-6e5f-429a-926f-9d8b3e5d2c4f', 'address7', 'Demo contact 7', 'demo7@gmail.com', b'0', 'linktr.ee/demo7', 'Grace', '3210987654', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo7', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('bc5ef2d3-7c6d-4b88-9e3f-4a58b2f6d78c', 'address8', 'Demo contact 8', 'demo8@gmail.com', b'1', 'linktr.ee/demo8', 'Hannah', '2109876543', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo8', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('d8a4c6ef-8e7f-4b3d-963e-8d7b2e6f78d9', 'address9', 'Demo contact 9', 'demo9@gmail.com', b'0', 'linktr.ee/demo9', 'Ian', '1098765432', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo9', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('e9b26f8d-5e48-47af-9e25-1d9b5d1f9b7d', 'address10', 'Demo contact 10', 'demo10@gmail.com', b'1', 'linktr.ee/demo10', 'Jack', '9876543201', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo10', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('f1a9e3c0-4e98-4f8d-b4e5-2e59c0272b34', 'address11', 'Demo contact 11', 'demo11@gmail.com', b'0', 'linktr.ee/demo11', 'Karen', '8765432019', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo11', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('ab3de2ef-6f5e-439b-936f-8f9b3f5d3d5f', 'address12', 'Demo contact 12', 'demo12@gmail.com', b'1', 'linktr.ee/demo12', 'Leo', '7654320198', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo12', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('bc6ef3f4-7d6f-4c89-a3f0-4b68c3f7f89d', 'address13', 'Demo contact 13', 'demo13@gmail.com', b'0', 'linktr.ee/demo13', 'Mia', '6543209871', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo13', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('d9b5e7ef-8f8f-4c4e-974f-9f8b3f7f89e9', 'address14', 'Demo contact 14', 'demo14@gmail.com', b'1', 'linktr.ee/demo14', 'Nina', '5432098762', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo14', '689444e0-6000-4107-99df-d8335e4f9a8c'),
+
+('e9b36f8e-5f49-48af-a2f6-2f9b5f2f0b8f', 'address15', 'Demo contact 15', 'demo15@gmail.com', b'0', 'linktr.ee/demo15', 'Oscar', '4321098761', 
+'https://static.vecteezy.com/system/resources/thumbnails/020/765/399/small_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg', 
+'linktr.ee/demo15', '689444e0-6000-4107-99df-d8335e4f9a8c');
